af70f9e7761ed47f91637ec0a4a657a876a43ac7
Kevan <44446640+kevankas@users.noreply.github.com>
2023-08-14 23:51:33 -0500

    basically finished maybe change color of winning lines?

--- script.js
+++ script.js
@@ -119,8 +119,85 @@ function restartingGame()
 
 function checkGameOver()
 {
-    // if (tile.innerText == "X")
-    // {
-    //     console.log("gameover");
-    // }
+ for (let row = 0; row < 3; row++)//horizontal check
+ {
+    if ((board[row][0] == board[row][1]) && (board[row][1] == board[row][2]) && (board[row][0] != ""))
+    {
+        if (board[row][0] == "X")
+        {
+            let gg = document.createElement("div");
+            document.getElementById("topUI").append(gg);
+            gg.innerText = "Game Over! X WINS!";
+            gg.classList.add("gamedisplay");
+        } 
+        else
+        {
+            let gg = document.createElement("div");
+            document.getElementById("topUI").append(gg);
+            gg.innerText = "Game Over! O WINS!";
+            gg.classList.add("gamedisplay");
+        }
+        gameOver = true;
+    }
+ }
+ for (let column = 0; column < 3; column++)//vertical check
+ {
+    if ((board[0][column] == board[1][column]) && (board[1][column] == board[2][column]) && (board[0][column] != ""))
+    {
+        console.log("winnervertical");  
+        gameOver = true;
+        if (board[0][column] == "X")
+        {
+            let gg = document.createElement("div");
+            document.getElementById("topUI").append(gg);
+            gg.innerText = "Game Over! X WINS!";
+            gg.classList.add("gamedisplay");
+        } 
+        else
+        {
+            let gg = document.createElement("div");
+            document.getElementById("topUI").append(gg);
+            gg.innerText = "Game Over! O WINS!";
+            gg.classList.add("gamedisplay");
+        }
+    }
+ }
+ if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[0][0] != "") //diag winner
+ {
+    if (board[0][0] == "X")
+    {
+        let gg = document.createElement("div");
+        document.getElementById("topUI").append(gg);
+        gg.innerText = "Game Over! X WINS!";
+        gg.classList.add("gamedisplay");
+    } 
+    else
+    {
+        let gg = document.createElement("div");
+        document.getElementById("topUI").append(gg);
+        gg.innerText = "Game Over! O WINS!";
+        gg.classList.add("gamedisplay");
+    }
+    console.log("diagwinner")
+    gameOver = true;
+ }
+ if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && board[0][2] != "") //diag winner
+ {
+    if (board[0][2] == "X")
+    {
+        let gg = document.createElement("div");
+        document.getElementById("topUI").append(gg);
+        gg.innerText = "Game Over! X WINS!";
+        gg.classList.add("gamedisplay");
+    } 
+    else
+    {
+        let gg = document.createElement("div");
+        document.getElementById("topUI").append(gg);
+        gg.innerText = "Game Over! O WINS!";
+        gg.classList.add("gamedisplay");
+    }
+    console.log("diagwinnerx2")
+    gameOver = true;
+ }
 }
